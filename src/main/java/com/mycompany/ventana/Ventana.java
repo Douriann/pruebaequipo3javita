6f705f63228439c6e17c1becdfe07c7b6b3b8fa4
Douriann <scoclox@gmail.com>
2024-06-28 19:24:12 -0400

    primer archivo

---
+++ src/main/java/com/mycompany/ventana/Ventana.java
@@ -0,0 +1,11 @@
+package com.mycompany.ventana;
+import principal.Principalita;
+
+public class Ventana {
+
+    public static void main(String[] args) {
+        Principalita ventana = new Principalita();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
